1de82e8b4e6f9093c062dc324a0cacc417f8520e
fredjacobs <thefredjacobs@gmail.com>
2023-02-14 21:05:59 +0200

    Done First 3 Tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(word,n) {
 
+    let str = '';
+
+    if(Math.sign(n) === -1){
+        return str = 'ERROR';
+    }
+    for(let i = 1; i <= n; i++){
+
+        
+        str += word;
+        
+    }
+    return str;
 };
 
 // Do not edit below this line
