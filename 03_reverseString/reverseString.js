021afb7fa4fb0e8fbdc13a0512337bfbaf810376
fredjacobs <thefredjacobs@gmail.com>
2023-02-15 16:49:55 +0200

    Lesson 3 half complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
+    const getArray = word.split('');
+    getArray.reverse();
+    const reversString = getArray.join('');
+    return reversString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
 const removeFromArray = function() {
+    
+    const args = [...arguments];
+    console.log(`Original Args ${args}`);
+    let primeArr = args[0];
+    console.log(`Array to be changed: ${primeArr}`);
+    args.shift();
+    console.log(`Values to remove: ${args}`);
+    let finalArr = primeArr.filter(el => !args.includes(el));
+    console.log(`Final Array ${finalArr}`);
 
-};
+    
+  
+
+    return finalArr;
+    
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
