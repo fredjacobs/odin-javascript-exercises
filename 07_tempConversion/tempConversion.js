4e9c3428a23f7aee265de4202540c49a2b54043d
fredjacobs <thefredjacobs@gmail.com>
2023-02-16 14:30:02 +0200

    Done Test 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+
+   const cel = (f - 32) * 5/9;
+   return parseFloat(Number(cel.toFixed(1)));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  
+  const far = (c * 9/5) + 32;
+  return parseFloat(Number(far.toFixed(1)));
 };
 
 // Do not edit below this line
