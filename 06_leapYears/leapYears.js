5143639fa7ea5b1fde969603a5bef3b7f6ba9a5b
fredjacobs <thefredjacobs@gmail.com>
2023-02-16 13:48:22 +0200

    Done Test 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,19 @@
-const leapYears = function() {
+const leapYears = function(argYear) {
+
+    const year = argYear;
+    let contender = null;
+    let isLeapYear = null;
+
+   if (year % 4 == 0 && year % 100 != 0){
+    return isLeapYear = true;
+   
+   }
+
+   if(year % 4 == 0 && year % 100 == 0 && year % 400 == 0){
+    return isLeapYear = true;
+   }
+
+    return isLeapYear = false;
 
 };
 
