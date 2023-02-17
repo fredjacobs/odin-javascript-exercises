ed5b263b135678b603fee116e3ce6a24c0966aba
fredjacobs <thefredjacobs@gmail.com>
2023-02-17 11:36:25 +0200

    Done Test 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,13 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
 const subtract = function() {
+  let num1 = arguments[0];
+  let num2 = arguments[1];
+
+  return num1 - num2;
 	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  
+	let answer = 0;
+ 
+  
+
+  
+
+  if (arr.length == 0){
+    return 0;
+  }
+
+  for(let i = 0; i < arr.length; i++){
+    answer += arr[i];
+    console.log(arr[i]);
+  }
+  return answer;
 };
 
 const multiply = function() {
