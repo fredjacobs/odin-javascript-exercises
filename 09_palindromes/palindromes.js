f6c847a1b8aa1ee5bb5383dab992f9403a4f4724
fredjacobs <thefredjacobs@gmail.com>
2023-02-17 16:47:16 +0200

    Done Test 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,26 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+let lowerStr  = str.toLowerCase();    
+
+let cleanStr = lowerStr.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").replace(/\s/g, "");
+
+console.log(cleanStr);
+
+
+let revStr = "";
+for (let i = cleanStr.length - 1; i >= 0; i--){
+    revStr += cleanStr[i];
+}
+
+const isPalindrome = revStr.localeCompare(cleanStr);
+
+if (isPalindrome == 0){
+    return true;
+}else{
+    return false;
+}
+
+
 
 };
 
