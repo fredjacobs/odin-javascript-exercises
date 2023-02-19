44a22653ad498ffb3ed0957b124737c5f7fb2bf3
fredjacobs <thefredjacobs@gmail.com>
2023-02-19 17:01:37 +0200

    Done Test 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    let age = 0;
+    let oldestPerson = {};
+
+    people.forEach(person => {
+
+        if(!person.yearOfDeath){
+            let curYear = new Date();
+            person.yearOfDeath = curYear.getFullYear();
+        }
+
+       let personAge = person.yearOfDeath - person.yearOfBirth;
+       
+       if (personAge > age){
+        oldestName = person.name;
+        age = personAge
+        oldestPerson = person;
+        console.log(age);
+       }
+        
+    });
+    
+    return oldestPerson;
 };
 
 // Do not edit below this line
